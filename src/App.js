5878e24177913c847535708f6d427ba634a8c759
bronzlabec <aurelian.ardeleanu@gmail.com>
2019-01-01 20:05:25 +0000

    random background using inline styles

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  backArr = ['--pink', '--green', '--yellow', '--lila'];
+  backStyle = {
+    backgroundColor: this.getColor()
+  };
+  
+  getColor() {
+    let rand = Math.floor(Math.random() * 4);
+    return `var(${this.backArr[rand]})`;
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="App" style={this.backStyle} >
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edited.
+            Edited today.
           </p>
         </header>
       </div>
